7f1a17444abf7b126f4dce40b61e4713fe9c471c
LenaSchluter <lena.sn8@gmail.com>
2025-10-28 10:51:28 +0100

    k

--- tests/testthat.R
+++ tests/testthat.R
@@ -11,28 +11,3 @@ library(centdog)
 
 test_check("centdog")
 
-test_that("extract_codons splits sequence into correct codons", {
-
-  # Example input
-  seq <- "ATGCGTAAATTT"
-
-  # Expected output (vector of codons)
-  expected_codons <- c("ATG", "CGT", "AAA", "TTT")
-
-  # Test equality
-  expect_equal(extract_codons(seq), expected_codons)
-
-  # Test different starting frames
-  expect_equal(extract_codons(seq, start = 2), c("TGC", "GTA", "AAT"))
-  expect_equal(extract_codons(seq, start = 3), c("GCG", "TAA", "ATT"))
-
-  # Edge case: sequence not divisible by 3
-  seq2 <- "ATGCGTA"
-  expect_equal(extract_codons(seq2), c("ATG", "CGT"))
-
-  # Edge case: very short sequence
-  expect_equal(extract_codons("AT"), character(0))
-})
-
-
-
--- tests/testthat/test-extract_codons.R
+++ tests/testthat/test-extract_codons.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("extract_codons splits sequence into correct codons", {
+
+  # Example input
+  seq <- "ATGCGTAAATTT"
+
+  # Expected output (vector of codons)
+  expected_codons <- c("ATG", "CGT", "AAA", "TTT")
+
+  # Test equality
+  expect_equal(extract_codons(seq), expected_codons)
+
+  # Test different starting frames
+  expect_equal(extract_codons(seq, start = 2), c("TGC", "GTA", "AAT"))
+  expect_equal(extract_codons(seq, start = 3), c("GCG", "TAA", "ATT"))
+
+  # Edge case: sequence not divisible by 3
+  seq2 <- "ATGCGTA"
+  expect_equal(extract_codons(seq2), c("ATG", "CGT"))
+
+  # Edge case: very short sequence
+  expect_equal(extract_codons("AT"), character(0))
 })
+
